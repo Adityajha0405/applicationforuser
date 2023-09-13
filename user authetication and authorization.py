16d9403a6bacf4115f86609b5fe6d68c1b4ee8d2
Adityajha0405 <84632075+Adityajha0405@users.noreply.github.com>
2023-09-13 21:18:00 +0530

    Add files via upload

---
+++ user authetication and authorization.py
@@ -0,0 +1,76 @@
+# Django Web Application with User Authentication and Authorization
+
+# Import necessary modules
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+# Create Flask app
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+
+# Configure database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+# Configure login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# User model
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Routes
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user:
+            flash('Username already exists!')
+            return redirect(url_for('signup'))
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Account created successfully!')
+        return redirect(url_for('login'))
+    return render_template('signup.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for('profile'))
+        flash('Invalid username or password!')
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', username=current_user.username)
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
